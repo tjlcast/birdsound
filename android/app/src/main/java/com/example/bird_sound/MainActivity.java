5f626d7ab5566bfc0a465994d8ea05b74da4b2e1
tjlcast <phx108@sina.com>
2026-04-20 14:36:18 +0800

    feat: add status bar plugin and styling

--- android/app/src/main/java/com/example/bird_sound/MainActivity.java
+++ android/app/src/main/java/com/example/bird_sound/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bird_sound;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -13,6 +14,8 @@ public class MainActivity extends BridgeActivity {
         super.onStart();
         WebView webView = getBridge().getWebView();
         WebSettings webSettings = webView.getSettings();
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         // http调用
         webSettings.setMixedContentMode(webSettings.MIXED_CONTENT_ALWAYS_ALLOW);
     }
